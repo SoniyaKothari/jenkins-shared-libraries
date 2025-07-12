90bdf80166d07590bf783e514fbb9bb67e262448
Soniya Kothari <soniyakothari1511@gmail.com>
2025-07-12 17:21:49 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser)
 {
-  sh "docker build -t ${DockerHubUser}/{ProjectName}:{ImageTag} . "
+  sh "docker build -H unix:///var/run/docker.sock -t ${DockerHubUser}/{ProjectName}:{ImageTag} . "
 }
