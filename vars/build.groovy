2363df1548b725c281efc5ac2b4b770365f26552
Soniya Kothari <soniyakothari1511@gmail.com>
2025-07-12 17:31:24 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,10 +1,4 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser) 
+def call(String ProjectName, String ImageTag, String DockerHubUser)
 {
-    def imageFullName = "${DockerHubUser}/${ProjectName}:${ImageTag}"
-    echo "Building Docker image: ${imageFullName}"
-
-    withEnv(["DOCKER_HOST=unix:///var/run/docker.sock"]) 
-    {
-        sh "docker build -t ${imageFullName} ."
-    }
-}
+  sh "docker -t ${DockerHubUser}/{ProjectName}:{ImageTag} . "
+} 
